fe74c01c84bcfd8edf63b9f1d1f0f29ad5234cf0
YulianSan <knoob0733@gmail.com>
2026-03-07 12:59:56 -0300

    chore: add ziggy for frontend route

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import '../css/app.css';
+import { Ziggy } from './ziggy'
+import { route } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,9 +16,16 @@ createInertiaApp({
             import.meta.glob<DefineComponent>('./pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
+        const vueApp = createApp({ render: () => h(App, props) });
+
+        vueApp.config.globalProperties.$route = (
+            name: string,
+            params?: any,
+        ) => route(name, params, undefined, Ziggy)
+
+        vueApp.use(plugin)
             .mount(el);
+
     },
     progress: {
         color: '#4B5563',
