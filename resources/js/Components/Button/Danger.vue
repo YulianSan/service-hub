3890172f99ece0932fc70f2e120d0572fa821aa4
YulianSan <knoob0733@gmail.com>
2026-03-07 21:08:43 -0300

    refactor(components.button): add cursor pointer

--- resources/js/Components/Button/Danger.vue
+++ resources/js/Components/Button/Danger.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="bg-red-700 hover:bg-red-800 text-white font-semibold py-2 px-4 rounded shadow transition-colors focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
+        class="bg-red-700 hover:bg-red-800 text-white font-semibold py-2 px-4 rounded shadow transition-colors focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 cursor-pointer">
         <slot />
     </button>
 </template>
--- resources/js/Components/Button/Primary.vue
+++ resources/js/Components/Button/Primary.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded shadow transition-colors focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-offset-2">
+        class="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded shadow transition-colors focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-offset-2 cursor-pointer">
         <slot />
     </button>
 </template>
